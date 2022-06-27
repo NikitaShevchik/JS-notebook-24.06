67bbcfdb317216b9741ed1140fcdd7c13974e52f
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-27 16:02:08 +0300

    27

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,1 +1,80 @@
 "use strict"
+
+const saveNoteButton = document.querySelector('.notebook__button');
+const textArea = document.querySelector('.notebook__textarea');
+const notesList = document.querySelector('.note__items');
+const newBoteButton = document.querySelector('.notebook__newitem');
+
+const noteSaves = [];
+
+saveNoteButton.addEventListener('click', saveNote);
+
+function saveNote() {
+    // noteSaves.push(textArea.value)
+
+    saverNote(notesList.children.length + 1, textArea.value);
+
+    notesList.innerHTML += `<li class="notebook__item" id="${notesList.children.length + 1}">Запись ${notesList.children.length + 1}</li>`
+
+    console.log(noteSaves)
+}
+
+
+function saverNote(key, value) {
+    noteSaves.push({ key, value });
+}
+
+
+
+notesList.addEventListener('click', function (e) {
+    switchNote(e);
+    openNote(e, e.target.id);
+    // console.log(e.target)
+    // console.log(e.target.id)
+    // for (let i of notesList.children) {
+    //     if (e.target == i) {
+    //         i.classList.toggle('_active');
+    //     } else {
+    //         i.classList.remove('_active')
+    //     }
+    // }
+    // e.target.classList.toggle('_active');
+
+    // textArea.value = noteSaves[e.target.id - 1]
+})
+
+// notesList.children.addEventListener('click', function(){
+//     console.log('YES')
+// })
+
+
+newBoteButton.addEventListener('click', newEmptyNote);
+function newEmptyNote() {
+    textArea.value = '';
+    notesList.innerHTML += `<li class="notebook__item" id="${notesList.children.length + 1}">Запись ${notesList.children.length + 1}</li>`
+    saverNote(notesList.children.length + 1, textArea.value);
+    console.log(noteSaves)
+}
+
+
+function openNote(target, id) {
+    const numberNote = id;
+    for (let k of noteSaves) {
+        if (k.key == numberNote) {
+            textArea.value = k.value;
+        }
+    }
+}
+
+
+
+/*--------------------Switcher for notes. Active note class = _active---------------------*/
+function switchNote(e) {
+    for (let i of notesList.children) {
+        if (e.target == i) {
+            i.classList.toggle('_active');
+        } else {
+            i.classList.remove('_active')
+        }
+    }
+}
